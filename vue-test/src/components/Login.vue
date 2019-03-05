c988ba64d52560c69ccb92147fe96054c1d6286f
Jacob Larsson <jacla460@student.liu.se>
2019-03-05 11:23:46 +0100

    Changed styling of login page

--- vue-test/src/components/Login.vue
+++ vue-test/src/components/Login.vue
@@ -2,11 +2,14 @@
     <div class="login-wrapper border border-light">
         <form class="form-signin" @submit.prevent="login">
             <h2 class="form-signin-heading">Please sign in</h2>
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Eamil address" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+            <section>
+            <label for="inputEmail" class="sr-only">Email address</label> <br/>
+            <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            </section><section>
+            <label for="inputPassword" class="sr-only">Password</label> <br/>
+            <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required> <br/>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </section>
         </form>
     </div>
 </template>
@@ -33,10 +36,18 @@ export default {
 h2{
     color: #3CB371;
     font-weight: bold;
+    font-size: 24px;
+}
+section{
+    padding: 10px;
 }
 label{
+    font-size: 18px;
     color: #3CB371;
     padding: 7px;
+}
+button{
+    margin: 12px;
 }
 </style>
 
