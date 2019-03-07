60c85a7a46c2ff17e8f84297df9762069c85218e
Yousef Hashem <youha847@student.liu.se>
2019-03-07 17:38:38 +0100

    add create user button

--- vue-test/src/components/LogIn.vue
+++ vue-test/src/components/LogIn.vue
@@ -1,34 +1,63 @@
 <template>
     <div class="login-wrapper border border-light">
-        <form class="form-signin" @submit.prevent="login">
-            <h2 class="form-signin-heading">Please sign in</h2>
+        <form class="form-signin" v-on:submit.prevent="login">
+            <h2 class="form-signin-heading">
+            Please sign in
+            </h2>
+
             <section>
-            <label for="inputEmail" class="sr-only">Email address</label> <br/>
+            <label for="inputEmail" class="sr-only"> Email address 
+            </label> 
+            <br/>
+
             <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autgofocus>
-            </section><section>
-            <label for="inputPassword" class="sr-only">Password</label> <br/>
-            <input v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required> <br/>
-            <button class="btn btn-lg btn-primary btn-block" type="submit"
-            v-on:release="login()">Sign in</button>
             </section>
+
+            <section>
+            <label for="inputPassword" class="sr-only">
+            Password
+            </label> 
+            <br/>
+
+            <input v-model="password" type="password" id="inputPassword"
+            class="form-control" placeholder="Password" required> 
+            <br/>
+            </section> 
+
+            <button class="btn btn-lg btn-primary btn-block" type="submit" v-on:release="login">
+            Sign in
+            </button>
+
+            <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="create">
+            Create user
+            </button>
+
         </form>
+
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Login',
-    data() {
-        return{
+    name: 'LogIn',
+    data: function(){
+        return {
             email: '',
-            password:''
+            password:'',
+            creating: false,
         }
     },
     methods: {
-        login(){
+        login: function(){
             /* eslint-disable no-console */
             console.log("email:", this.email);
             console.log("pw:", this.password);
+            /* eslint-enable no-console */
+        },
+        create: function(){
+            this.creating = !this.creating;
+            /* eslint-disable no-console */
+            console.log(this.creating);
             /* eslint-enable no-console */
         }
     }
@@ -49,11 +78,11 @@ label {
     color: #4a86e8;
 }
 button {
-    margin: 12px;
+    margin: 10px;
     font-size: 100%;
     border-radius: 30px 10px;
-    padding-left: 25px;
-    padding-right: 25px;
+    padding-left: 15px;
+    padding-right: 15px;
     padding-top: 5px;
     padding-bottom: 5px;
     border: 2px solid #4a86e8;
