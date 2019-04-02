3a408fd60d2f1485d48e73b596696d37e72b07e0
Jacob Larsson <jacla460@student.liu.se>
2019-04-02 09:12:12 +0200

    Added functionality to continously track the users position

--- front-end/src/components/MapBase.vue
+++ front-end/src/components/MapBase.vue
@@ -6,6 +6,53 @@
 <script>
 import gmapsInit from '../utils/gmaps.js'
 
+let positionTimer;
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos, map) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? 'Error: Geolocation misslyckades!' : 'Error: Din webbläsare stödjer inte geolocation');
+    infoWindow.open(map);
+}
+
+function geoLocate(map, infoWindow){
+    navigator.geolocation.getCurrentPosition(function(position) {
+         let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Här är du.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+        return position;
+    }, function() {
+        // Geolocation ej tillåtet
+        handleLocationError(true, infoWindow, map.getCenter(), map);
+    });
+}
+
+function sendPostition(pos){
+    // Skriv kod här som skickar "position" till backend
+}
+
+function watchCurrentPosition(infoWindow){
+    positionTimer = navigator.geolocation.watchPosition(
+        function(position){
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            infoWindow.setPosition(pos);
+            sendPostition(pos);
+        }
+    ,null,
+    {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: Infinity
+    });
+}
+
 export default {
     name: 'Map',
     async mounted() {
@@ -24,7 +71,15 @@ export default {
         let infoWindow = new google.maps.InfoWindow;
         
         if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(function(position) {
+            let position = geoLocate(map, infoWindow);
+            watchCurrentPosition(infoWindow, position);
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: 'Hello World!',
+                icon: 'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png',
+            });
+            /*navigator.geolocation.getCurrentPosition(function(position) {
                 let pos = {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
@@ -36,16 +91,11 @@ export default {
                 map.setCenter(pos);
             }, function() {
                 // Geolocation ej tillåtet
-                handleLocationError(true, infoWindow, map.getCenter());
-            });
+                handleLocationError(true, infoWindow, map.getCenter(), map);
+            });*/
         } else {
             // Geolocation ej activerat
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-            infoWindow.setPosition(pos);
-            infoWindow.setContent(browserHasGeolocation ? 'Error: Geolocation misslyckades!' : 'Error: Din webbläsare stödjer inte geolocation');
-            infoWindow.open(map);
+            handleLocationError(false, infoWindow, map.getCenter(), map);
         }
         });
     },
