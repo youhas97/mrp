3f23ad2b89b93688ff3d57a480bf411d63cba151
Elmedin Zildzic <elmzi904@student.liu.se>
2019-04-03 23:25:14 +0200

    Enabling node debugging.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -5,21 +5,6 @@
 </template>
 
 <script>
-    let that = this;
-
-    var socket = new WebSocket('ws://127.0.0.1:9000/ws/connect/');
-
-    socket.onclose = function(event) {
-        /* eslint-disable no-console */
-        console.error('Socket closed unexpectedly!', event.code);
-        /* eslint-enable no-console */
-    };
-
-    socket.onopen = function() {
-        /* eslint-disable no-console */
-        console.error('Socket opened!', event.code);
-        /* eslint-enable no-console */
-    };
 </script>
 
 <style>
