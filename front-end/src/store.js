ee550ebd3b4dbe6d1c7297d3da042aad312929e4
Yousef Hashem <youha847@student.liu.se>
2019-04-05 12:24:49 +0200

    Fix merge error

--- front-end/src/store.js
+++ front-end/src/store.js
@@ -9,24 +9,17 @@ In this case, we can use the same websocket in all of our components.
 'websocket' is first assigned a WebSocket in LogIn.vue.
 */
 export default new Vuex.Store({
-<<<<<<< HEAD
-  state: {
-    websocket: null,
-    meObj: {
-      "id": null,
-      "pos": null,
-      "name": null,
-      "group": null,
-      "needHelp": null
-  }
-  },
-  mutations: {
-=======
     state: {
-        websocket: null
+        websocket: null,
+        meObj: {
+            "id": null,
+            "pos": null,
+            "name": null,
+            "group": null,
+            "needHelp": null
+        }
     },
     mutations: {
->>>>>>> origin/feature/channellayer
 
     },
     actions: {
