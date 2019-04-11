dee2a0d01435f8d2224ee342eedbb22e892e2054
Yousef Hashem <youha847@student.liu.se>
2019-04-11 15:31:37 +0200

    add Mobile.vue

---
+++ front-end/src/components/Mobile.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="mobile">
+        <Larm/>
+        <Map/>
+    </div>
+</template>
+
+<script>
+import Larm from './Larm'
+import Map from './MapBase'
+
+export default {
+    name: 'Mobile',
+    components: {
+        Larm,
+        Map
+    },
+}
+</script>
+
+<style scoped>
+.Map {
+    width: 99vw;
+    height: 90vh;
+    margin: auto;
+}
+</style>
+
