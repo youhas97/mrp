8b6cf5b75172289c481bd060595b165a78992eed
Elmedin Zildzic <elmzi904@student.liu.se>
2019-04-11 12:41:32 +0200

    Fixed http redirect to https for the node server.

--- front-end/server.js
+++ front-end/server.js
@@ -5,7 +5,16 @@ const serveStatic = require("serve-static");
 const path = require('path');
 
 app = express();
+
+app.use(
+    (req, res, next) => {
+    if(req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`);
+    else
+        next();
+});
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 8000;
-app.listen(port)
+app.listen(port);
