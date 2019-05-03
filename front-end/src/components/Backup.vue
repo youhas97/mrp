c8e045b28a524708be250678d0ad3377aed3ecd4
Elmedin Zildzic <elmzi904@student.liu.se>
2019-05-03 16:42:10 +0200

    Changed the search function to a button with dropdown menu on available users.

--- front-end/src/components/Backup.vue
+++ front-end/src/components/Backup.vue
@@ -5,10 +5,11 @@
         {{buttonText}}
         </button>
 
-        <form id="searchForm" @submit.prevent="searchUser">
-            <input v-model="searchText" type="text" placeholder="Search user...">
-            <button type="submit" @release="searchUser">Search</button>
-        </form>
+        <div id="userSelection" class="dropdownList">
+            <button class="dropdownBtn" type="submit" @mousedown="searchUser">Find user</button>
+            <div id="dropdown" class="dropdown-content">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -30,7 +31,19 @@ export default {
             searchText: ""
         }
     },
-
+    mounted() {
+        window.onclick = (event) => {
+            if(!event.target.matches('.dropdownBtn')) {
+                /* If click event is outside dropdownBtn, then hide dropdown div
+                and delete child elements */
+                var dropdownList = document.getElementById('dropdown');
+                if(dropdownList.classList.contains('show'))
+                    dropdownList.classList.remove('show');
+                while(dropdownList.firstChild) 
+                    dropdownList.removeChild(dropdownList.firstChild);
+            }
+        };
+    },
     methods: {
         hold: function() {
             var secs = 3;
@@ -73,12 +86,40 @@ export default {
             }
         },
         searchUser: function() {
-            var user = this.$store.state.users.allUsers[this.searchText];
-            if(!user)
-                alert("User does not exist.");
-            else {
-                alert("User found!");
-                this.$root.$emit('locateUser', this.searchText);
+            /* This function is executed by the dropdown button */
+            document.getElementById('dropdown').classList.toggle('show');
+
+            /* If the dropdown list already contains elements, 
+                don't add duplicates. */
+            if(!document.getElementById('dropdown').firstChild) {
+                // Add all users to the dropdown div. 
+                for(var user in this.$store.state.users.allUsers){
+                    var dropdown = document.getElementById('dropdown');
+                    var userButton = document.createElement('button');
+                    // styling.
+                    userButton.style.padding = "12px 16px";
+                    userButton.style.width = "100%";
+                    userButton.style.color = "#000000"
+                    userButton.style.display = "block";
+                    userButton.style.border = "none";
+                    userButton.style['background-color'] = "#e7e7e7";
+                    userButton.innerHTML = user;
+                    // add click listener that locates user that is clicked.
+                    userButton.addEventListener('click', (event) => {
+                        let username = event.srcElement.innerHTML;
+                        this.$root.$emit('locateUser', username);
+                    });
+                    dropdown.appendChild(userButton);
+                }
+            } else if(
+                !document.getElementById('dropdown').classList.contains('show') &&
+                document.getElementById('dropdown').firstChild
+            ) {
+                /* If dropdownList is hidden with toggle, 
+                delete all children element */
+                var dropdownList = document.getElementById('dropdown');
+                while(dropdownList.firstChild)
+                    dropdownList.removeChild(dropdownList.firstChild);
             }
         }
         /*
@@ -105,5 +146,38 @@ export default {
     padding-top: 5px;
     padding-bottom: 5px;
     border: 2px solid #4a86e8;
+}
+
+#userSelection {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 100px;
+    overflow: auto;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.show {
+    display: block;
+}
+
+.dropdown-content button {
+    display: block;
+    padding: "12px 16px";
+    width: 100%;
+    color: black;
+    background-color: "#e7e7e7";
+    border: none;
+}
+
+.dropdown-content button:hover {
+    background-color: blue;
+    color: white;
 }
 </style>
